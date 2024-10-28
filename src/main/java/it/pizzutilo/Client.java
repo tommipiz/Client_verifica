6822613434ad178f0ff21e9e3debc0ebf34af9c8
tommipiz <tommaso.pizzutilo@gmail.com>
2024-10-28 13:23:33 +0100

    Client finish

--- src/main/java/it/pizzutilo/Client.java
+++ src/main/java/it/pizzutilo/Client.java
@@ -1,7 +1,45 @@
 package it.pizzutilo;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) throws UnknownHostException , IOException{
+       Socket s = new Socket("localHost", 3000);
+       System.out.println("Connessione al server effettuata");
+
+       try(BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String input;
+
+            while(true) {
+                System.out.println("Inserisci una nota , '?' per visualizzare le note salvate, o '!' per uscire: ");
+                input = userInput.readLine();
+
+                out.writeBytes(input + "\n");
+
+                if (input.equals("!")) {
+                    System.out.println("Disconnessione...");
+                    break;
+                } else if(input.equals("?")) {
+                    System.out.println("ecco la lista delle note salvate ");
+                    String response;
+                    while(!(response = in.readLine()).equals("@")) {
+                    System.out.println(response);
+                    }
+                } else {
+                    String response = in.readLine();
+                    System.out.println(("Risposta del server: " + response));
+                }
+            }   
+        } finally {
+            s.close();
+        }
     }
 }
